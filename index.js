0c5e1be78ac5905a58eb086aac941c08e8ecdf63
AlejoL <49210713+AlejoLovallo@users.noreply.github.com>
2021-01-01 18:09:46 -0300

    trying something new

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-module.exports = function tiny(string) {
+function tiny(string) {
     if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
     return string.replace(/\s/g, "");
-};
+}
+
+function hello(data){
+    console.log("Hello " + data);
+}
+
+module.exports = {
+    tiny,
+    hello,
+}
+
